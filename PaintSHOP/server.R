3695c94e35667cee5688348b282e7c87cd57262f
Elliot Hershberg <hershe@gs-104094.gs.washington.edu>
2019-07-15 17:37:40 -0700

    Add new features to side panel of app and added reactivity server side.

--- PaintSHOP/server.R
+++ PaintSHOP/server.R
@@ -3,9 +3,20 @@
 # July 15, 2019
 
 library(shiny)
+library(tidyverse)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+    # load in the probe set for the specified genome assembly
+    probes <- reactive({
+        read_delim(input$assembly,
+                   col_names = c("chrom", "start", "stop", 
+                                 "sequence", "Tm", "on-target",
+                                 "off-target", "repeat", "max-kmer",
+                                 "refseq"),
+                   delim = "\t")
+    })
+    
     # a reactive element that is a vector of the RefSeq IDs
     # if the user inputs them manually on the UI
     manual_accessions <- reactive({
@@ -13,22 +24,23 @@ shinyServer(function(input, output) {
         strsplit(input$refseq_manual, ", ")
     })
     
-    #
-    
-    
-    
-    
+    # read in a user's file and store it as a data frame w/
+    # a single row
+    accession_file <- reactive({
+        read_csv(input$refseq_file,
+                 col_names = c("refseq"))        
+    })
     
     output$plot <- renderPlot({
         plot(cars, type=input$plotType)
     })
     
     output$summary <- renderPrint({
-        summary(cars)
+        summary(probes())
     })
     
     output$table <- DT::renderDataTable({
-        DT::datatable(cars)
+        DT::datatable(head(probes()))
     })
 
 })
--- PaintSHOP/ui.R
+++ PaintSHOP/ui.R
@@ -3,10 +3,12 @@
 # July 15, 2019
 
 library(shiny)
+library(shinythemes)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
     
+    theme = shinytheme("flatly"),
     
     navbarPage("PaintSHOP",
       tabPanel("RNA Probe Design",
@@ -15,11 +17,11 @@ shinyUI(fluidPage(
             radioButtons("plotType", "Plot type",
                          c("Scatter"="p", "Line"="l")),
             radioButtons("assembly", "Choose Genome Assembly:",
-                         c("hg38"="hg38")),
+                         c("hg38"="/Users/hershe/Documents/OligoServer/probe_dbs/refseq_hg38_DNA-FISH_DB.csv")),
             textInput("refseq_manual", label = h3("Enter RefSeq IDs manually"), 
                       value = "NM_020309, NM_015267, NM_018008, NM_000818, ..."),
             tags$h3("OR"),
-            fileInput("refseq_file", label = "Upload a File With RefSeq IDs", 
+            fileInput("refseq_file", label = h3("Upload a File With RefSeq IDs"), 
                       multiple = FALSE, accept = NULL,
                       width = NULL, buttonLabel = "Browse...",
                       placeholder = "No file selected"),
@@ -44,7 +46,8 @@ shinyUI(fluidPage(
         )
       ),
       tabPanel("Summary",
-        verbatimTextOutput("summary")
+        #verbatimTextOutput("summary"),
+        DT::dataTableOutput("table")
       )
     )
     
