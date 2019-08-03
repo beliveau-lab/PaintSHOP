99aa7fae777eb5a258c716b0a035920de4f71aa3
Elliot Hershberg <hershe@gs-104094.gs.washington.edu>
2019-08-02 17:31:54 -0700

    Added a tab in the UI for DNA probe design, and worked on the server side for dealing with coordinates. Experimented with tidygenomics packages for handling coordinate intersect with data frames.

--- PaintSHOP/server.R
+++ PaintSHOP/server.R
@@ -8,6 +8,10 @@ library(vroom)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+    ##############################################
+    # RefSeq IDs
+    ##############################################
+    
     # load in the probe set for the specified genome assembly
     probes <- eventReactive(c(input$manual_submit,
                               input$refseq_file,
@@ -55,8 +59,6 @@ shinyServer(function(input, output) {
         merge(manual_accessions(), probes(), by="refseq")
     })
     
-    #probe_intersect <- reactiveVal(merge(manual_accessions(), probe_DB, by="refseq"))
-    
     # count number of probes for each ID
     probe_counts <- reactive({
         probe_intersect() %>%
@@ -78,5 +80,85 @@ shinyServer(function(input, output) {
     output$summary_table <- DT::renderDataTable({
         DT::datatable(head(probes()))
     })
+    
+    ##############################################
+    # Genomic Coordinates
+    ##############################################
+    
+    # load in the probe set for the specified genome assembly
+    probes <- eventReactive(c(input$manual_coord_submit,
+                              input$coord_file,
+                              input$coord_filter), {
+          # vroom makes a fast index instead of immediately loading
+          # every row
+          df <- vroom(input$probeset_coord,
+                      col_names = c("chrom", "start", "stop", 
+                                    "sequence", "Tm", "on_target",
+                                    "off_target", "repeat_seq", "max_kmer"),
+                      delim = "\t")
+          
+          if(input$repeat_seq) {
+              df %>%
+                  filter(off_target <= input$off_target,
+                         max_kmer <= input$max_kmer)
+          } else {
+              df %>%
+                  filter(repeat_seq != 1,
+                         off_target <= input$off_target,
+                         max_kmer <= input$max_kmer)
+          }
+      })
+    
+    # a reactive element that is a vector of the RefSeq IDs
+    # if the user inputs them manually on the UI
+    manual_coordinates <- eventReactive(input$manual_coord_submit, {
+        # split the manual comma separated input
+        manual_split <- unlist(strsplit(input$refseq_manual, " "))
+        
+        # create a dataframe w/ the accesions
+        tibble("" = manual_split)
+    })
+    
+    # read in a user's file and store it as a data frame w/
+    # a single row
+    accession_file <- reactive({
+        read_csv(input$refseq_file,
+                 col_names = c("refseq"))        
+    })
+    
+    # do intersection with RefSeq
+    probe_intersect <- reactive({
+        merge(manual_accessions(), probes(), by="refseq")
+    })
+    
+    # count number of probes for each ID
+    probe_counts <- reactive({
+        probe_intersect() %>%
+            group_by(refseq) %>%
+            count()
+    })
+    
+    # density plot of counts per prode
+    output$count_plot <- renderPlot({
+        ggplot(probe_counts(), aes(x = n)) +
+            geom_density() +
+            xlab("Number of probes per transcript")
+    })
+    
+    output$intersect_table <- DT::renderDataTable({
+        DT::datatable(probe_intersect())
+    })
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 })
--- PaintSHOP/ui.R
+++ PaintSHOP/ui.R
@@ -23,7 +23,7 @@ shinyUI(fluidPage(
                          c("hg38 newBalance" = "/Users/hershe/Documents/OligoServer/probe_dbs/refseq_hg38_DNA-FISH_DB.csv")),
             textInput("refseq_manual", label = h3("Enter RefSeq IDs manually"), 
                       value = "NM_020309, NM_015267, NM_018008, NM_000818, ..."),
-            actionButton("manual_submit", label = "Submit"),
+            actionButton("manual_coord_submit", label = "Submit"),
             tags$h3("OR"),
             fileInput("refseq_file", label = h3("Upload a File With RefSeq IDs"), 
                       multiple = FALSE, accept = NULL,
@@ -54,13 +54,6 @@ shinyUI(fluidPage(
                         min = 0, max = 255,
                         value = 5),
             actionButton("filter", label = "Filter")
-            
-            
-            
-            
-            
-            
-            
           ),
           mainPanel(
             plotOutput("count_plot"),
@@ -68,6 +61,53 @@ shinyUI(fluidPage(
             
           )
         )
+      ),
+      tabPanel("DNA Probe Design",
+               sidebarLayout(
+                 sidebarPanel(
+                   # change button values to path to complete newBalance probe set, not intersect
+                   radioButtons("probeset_coord", "Choose Probe Set:",
+                                c("hg38 newBalance" = "/Users/hershe/Documents/OligoServer/probe_dbs/refseq_hg38_DNA-FISH_DB.csv")),
+                   textInput("coordinate_manual", label = h3("Enter genomic coordinates manually "), 
+                             value = "chr10:26216307-26304562, chr12:111034165-111350554, chr3:62369681-62373550, chr10:26216307-26304562, ..."),
+                   actionButton("manual_coord_submit", label = "Submit"),
+                   tags$h3("OR"),
+                   fileInput("coord_file", label = h3("Upload a File With genomic coordinates"), 
+                             multiple = FALSE, accept = NULL,
+                             width = NULL, buttonLabel = "Browse...",
+                             placeholder = "No file selected"),
+                   tags$p("Please upload a text file with one genomic coordinate per line.",
+                          "For example:",
+                          tags$br(),
+                          tags$br(),
+                          "chr10:26,216,307-26,304,562",
+                          tags$br(),
+                          "chr12:111,034,165-111,350,554",
+                          tags$br(),
+                          "chr3:62,369,681-62,373,550",
+                          tags$br(),
+                          "chr10:26,216,307-26,304,562",
+                          tags$br(),
+                          "..."),
+                   tags$hr(),
+                   tags$h3("Probe Settings"),
+                   radioButtons("repeat_seq", "Repeat:",
+                                c("allowed" = TRUE, "none" = FALSE),
+                                selected = FALSE),
+                   sliderInput("off_target", "Off-Target Score:",
+                               min = 0, max = 1000,
+                               value = 200),
+                   sliderInput("max_kmer", "Max K-mer Count:",
+                               min = 0, max = 255,
+                               value = 5),
+                   actionButton("coord_filter", label = "Filter")
+                 ),
+                 mainPanel(
+                   plotOutput("count_plot"),
+                   DT::dataTableOutput("intersect_table")
+                   
+                 )
+               )
       ),
       tabPanel("Summary",
         DT::dataTableOutput("summary_table")
