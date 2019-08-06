0a69513a36f1ef1ddc7ef89df521c8d580240da0
Elliot Hershberg <hershe@gs-104094.gs.washington.edu>
2019-08-05 17:21:05 -0700

    Added example refseq and BED files to .gitignore. Finished my coordinate filtering script, and implemented the functionality in the DNA probe design tab in Shiny app. Also fixed the file upload vs. manual upload logic.

--- PaintSHOP/server.R
+++ PaintSHOP/server.R
@@ -5,6 +5,7 @@
 library(shiny)
 library(tidyverse)
 library(vroom)
+library(tidygenomics)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -13,9 +14,9 @@ shinyServer(function(input, output) {
     ##############################################
     
     # load in the probe set for the specified genome assembly
-    probes <- eventReactive(c(input$manual_submit,
-                              input$refseq_file,
-                              input$filter), {
+    probes_refseq <- eventReactive(c(input$refseq_submit,
+                                     input$refseq_file,
+                                     input$filter), {
         # vroom makes a fast index instead of immediately loading
         # every row
         df <- vroom(input$probeset,
@@ -39,24 +40,24 @@ shinyServer(function(input, output) {
     
     # a reactive element that is a vector of the RefSeq IDs
     # if the user inputs them manually on the UI
-    manual_accessions <- eventReactive(input$manual_submit, {
-        # split the manual comma separated input
-        manual_split <- unlist(strsplit(input$refseq_manual, ", "))
-        
-        # create a dataframe w/ the accesions
-        tibble("refseq" = manual_split)
-    })
-    
-    # read in a user's file and store it as a data frame w/
-    # a single row
-    accession_file <- reactive({
-        read_csv(input$refseq_file,
-                 col_names = c("refseq"))        
+    refseq_accessions <- eventReactive(input$refseq_submit, {
+        if(input$refseq_manual_or_file) {
+          # split the manual comma separated input
+          manual_split <- unlist(strsplit(input$refseq_manual, ", "))
+          
+          # create a dataframe w/ the accesions
+          tibble("refseq" = manual_split)
+        } else {
+          # read in a user's file and store it as a data frame w/
+          # a single row
+          read_csv(input$refseq_file$datapath,
+                   col_names = c("refseq"))
+        }
     })
     
     # do intersection with RefSeq
     probe_intersect <- reactive({
-        merge(manual_accessions(), probes(), by="refseq")
+      merge(refseq_accessions(), probes_refseq(), by="refseq")
     })
     
     # count number of probes for each ID
@@ -78,7 +79,7 @@ shinyServer(function(input, output) {
     })
     
     output$summary_table <- DT::renderDataTable({
-        DT::datatable(head(probes()))
+        DT::datatable(head(probes_refseq()))
     })
     
     ##############################################
@@ -86,9 +87,9 @@ shinyServer(function(input, output) {
     ##############################################
     
     # load in the probe set for the specified genome assembly
-    probes <- eventReactive(c(input$manual_coord_submit,
-                              input$coord_file,
-                              input$coord_filter), {
+    probes_full <- eventReactive(c(input$coord_submit,
+                                   input$coord_file,
+                                   input$coord_filter), {
           # vroom makes a fast index instead of immediately loading
           # every row
           df <- vroom(input$probeset_coord,
@@ -97,56 +98,83 @@ shinyServer(function(input, output) {
                                     "off_target", "repeat_seq", "max_kmer"),
                       delim = "\t")
           
-          if(input$repeat_seq) {
+          if(input$repeat_seq_coord) {
               df %>%
-                  filter(off_target <= input$off_target,
-                         max_kmer <= input$max_kmer)
+                  filter(off_target <= input$off_target_coord,
+                         max_kmer <= input$max_kmer_coord)
           } else {
               df %>%
                   filter(repeat_seq != 1,
-                         off_target <= input$off_target,
-                         max_kmer <= input$max_kmer)
+                         off_target <= input$off_target_coord,
+                         max_kmer <= input$max_kmer_coord)
           }
       })
     
     # a reactive element that is a vector of the RefSeq IDs
     # if the user inputs them manually on the UI
-    manual_coordinates <- eventReactive(input$manual_coord_submit, {
-        # split the manual comma separated input
-        manual_split <- unlist(strsplit(input$refseq_manual, " "))
+    coordinates <- eventReactive(input$coord_submit, {
+        if(input$coord_manual_or_file) {
+          # split the manual comma separated input
+          coord_split <- unlist(strsplit(input$coord_manual, ", "))
+          
+          chrom <- lapply(coord_split, function(x) unlist(strsplit(x, ":"))[1])
+          
+          coords <- lapply(coord_split, function(x) unlist(strsplit(x, ":"))[2])
+          
+          start <- lapply(coords, function(x) unlist(strsplit(x, "-"))[1])
+          stop <- lapply(coords, function(x) unlist(strsplit(x, "-"))[2])
+          
+          coord_df <- tibble("chrom" = unlist(chrom),
+                             "start" = unlist(start),
+                             "stop" = unlist(stop))
+          
+          coord_df <- coord_df %>% drop_na()
+          
+          # convert start stop to integers
+          coord_df$start <- as.numeric(coord_df$start)
+          coord_df$stop <- as.numeric(coord_df$stop)
+          
+          coord_df %>%
+            select(everything())
+        } else {
+          # read in a user's file and store it as a data frame w/
+          # rows to do intersect
+          read_tsv(input$coord_file$datapath,
+                   col_names = c("chrom",
+                                 "start",
+                                 "stop"))        
+        }
+      
         
-        # create a dataframe w/ the accesions
-        tibble("" = manual_split)
     })
     
-    # read in a user's file and store it as a data frame w/
-    # a single row
-    accession_file <- reactive({
-        read_csv(input$refseq_file,
-                 col_names = c("refseq"))        
-    })
-    
-    # do intersection with RefSeq
-    probe_intersect <- reactive({
-        merge(manual_accessions(), probes(), by="refseq")
+    # do intersection with coordinates
+    coord_intersect <- reactive({
+      #browser()
+      # I need to change to take either manual or file  
+      result <- genome_intersect(probes_full(), coordinates(), by = c("chrom", "start", "stop"))
+        
+        # reorder columns after intersect
+        result %>%
+          select(chrom, start, stop, everything())
     })
     
     # count number of probes for each ID
-    probe_counts <- reactive({
-        probe_intersect() %>%
-            group_by(refseq) %>%
+    coord_counts <- reactive({
+        coord_intersect() %>%
+            group_by(chrom) %>%
             count()
     })
     
     # density plot of counts per prode
-    output$count_plot <- renderPlot({
-        ggplot(probe_counts(), aes(x = n)) +
+    output$coord_count_plot <- renderPlot({
+        ggplot(coord_counts(), aes(x = n)) +
             geom_density() +
-            xlab("Number of probes per transcript")
+            xlab("Number of probes per genomic region")
     })
     
-    output$intersect_table <- DT::renderDataTable({
-        DT::datatable(probe_intersect())
+    output$coord_intersect_table <- DT::renderDataTable({
+        DT::datatable(coord_intersect())
     })
     
     
@@ -157,8 +185,4 @@ shinyServer(function(input, output) {
     
     
     
-    
-    
-    
-
 })
--- PaintSHOP/ui.R
+++ PaintSHOP/ui.R
@@ -12,7 +12,7 @@ shinyUI(fluidPage(
     
     # add CSS to the header to make horizontal rules visible
     tags$head(
-      tags$style(HTML("hr {border-top: 3px solid #000000;}"))
+      tags$style(HTML("hr {border-top: 3px solid #000000}"))
     ),
     
     navbarPage("PaintSHOP",
@@ -22,13 +22,16 @@ shinyUI(fluidPage(
             radioButtons("probeset", "Choose Probe Set:",
                          c("hg38 newBalance" = "/Users/hershe/Documents/OligoServer/probe_dbs/refseq_hg38_DNA-FISH_DB.csv")),
             textInput("refseq_manual", label = h3("Enter RefSeq IDs manually"), 
-                      value = "NM_020309, NM_015267, NM_018008, NM_000818, ..."),
-            actionButton("manual_coord_submit", label = "Submit"),
+                      value = "NM_020309, NM_015267, NM_018008, ..."),
             tags$h3("OR"),
             fileInput("refseq_file", label = h3("Upload a File With RefSeq IDs"), 
                       multiple = FALSE, accept = NULL,
                       width = NULL, buttonLabel = "Browse...",
                       placeholder = "No file selected"),
+            radioButtons("refseq_manual_or_file", "Input type:",
+                         c("Manual" = TRUE, "File" = FALSE),
+                         selected = FALSE),
+            actionButton("refseq_submit", label = "Submit"),
             tags$p("Please upload a text file with one RefSeq accession per line.",
                    "For example:",
                    tags$br(),
@@ -68,43 +71,46 @@ shinyUI(fluidPage(
                    # change button values to path to complete newBalance probe set, not intersect
                    radioButtons("probeset_coord", "Choose Probe Set:",
                                 c("hg38 newBalance" = "/Users/hershe/Documents/OligoServer/probe_dbs/refseq_hg38_DNA-FISH_DB.csv")),
-                   textInput("coordinate_manual", label = h3("Enter genomic coordinates manually "), 
-                             value = "chr10:26216307-26304562, chr12:111034165-111350554, chr3:62369681-62373550, chr10:26216307-26304562, ..."),
-                   actionButton("manual_coord_submit", label = "Submit"),
+                   textInput("coord_manual", label = h3("Enter genomic coordinates manually "), 
+                             value = "chr10:26216307-26304562, chr12:111034165-111350554, chr3:62369681-62373550, ..."),
                    tags$h3("OR"),
-                   fileInput("coord_file", label = h3("Upload a File With genomic coordinates"), 
+                   fileInput("coord_file", label = h3("Upload a file with genomic coordinates (BED format)"), 
                              multiple = FALSE, accept = NULL,
                              width = NULL, buttonLabel = "Browse...",
                              placeholder = "No file selected"),
-                   tags$p("Please upload a text file with one genomic coordinate per line.",
+                   radioButtons("coord_manual_or_file", "Input type:",
+                                c("Manual" = TRUE, "File" = FALSE),
+                                selected = FALSE),
+                   actionButton("coord_submit", label = "Submit"),
+                   tags$p("Please upload a BED file with one genomic coordinate per line.",
                           "For example:",
                           tags$br(),
                           tags$br(),
-                          "chr10:26,216,307-26,304,562",
+                          "chr10\t26216307\t26304562",
                           tags$br(),
-                          "chr12:111,034,165-111,350,554",
+                          "chr12\t111034165\t111350554",
                           tags$br(),
-                          "chr3:62,369,681-62,373,550",
+                          "chr3\t62369681\t62373550",
                           tags$br(),
-                          "chr10:26,216,307-26,304,562",
+                          "chr10\t26216307\t26304562",
                           tags$br(),
                           "..."),
                    tags$hr(),
                    tags$h3("Probe Settings"),
-                   radioButtons("repeat_seq", "Repeat:",
+                   radioButtons("repeat_seq_coord", "Repeat:",
                                 c("allowed" = TRUE, "none" = FALSE),
                                 selected = FALSE),
-                   sliderInput("off_target", "Off-Target Score:",
+                   sliderInput("off_target_coord", "Off-Target Score:",
                                min = 0, max = 1000,
                                value = 200),
-                   sliderInput("max_kmer", "Max K-mer Count:",
+                   sliderInput("max_kmer_coord", "Max K-mer Count:",
                                min = 0, max = 255,
                                value = 5),
                    actionButton("coord_filter", label = "Filter")
                  ),
                  mainPanel(
-                   plotOutput("count_plot"),
-                   DT::dataTableOutput("intersect_table")
+                   plotOutput("coord_count_plot"),
+                   DT::dataTableOutput("coord_intersect_table")
                    
                  )
                )
